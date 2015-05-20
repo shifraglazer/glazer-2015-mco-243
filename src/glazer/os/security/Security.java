3e3349f20836fb87741022cc2c5d5fb680de873d
a <a>
2015-05-19 22:23:01 -0400

    security

---
+++ src/glazer/os/security/Security.java
@@ -0,0 +1,12 @@
+package glazer.os.security;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class Security {
+
+	public static void main(String args[]){
+		
+		System.out.println(DigestUtils.md5Hex("I will graduate soon"));
+	
+	}
+}
